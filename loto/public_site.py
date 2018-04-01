46f9fea9cfc186edc57a6857cfb323f42a844da6
JuuichiHirao <hirao@theta-inc.co.jp>
2018-04-01 17:11:10 +0900

    before delete setting LotoData

--- loto/public_site.py
+++ loto/public_site.py
@@ -4,12 +4,13 @@ import os
 import codecs
 import re
 import csv
+from datetime import datetime
 
 
-class Loto:
+class LotoData:
+
     def __init__(self):
-        self.max_time = 0
-        # self.lottery_date
+        self.lottery_date = datetime.strptime('1900/1/1', '%Y/%m/%d')
         self.times = 0
         self.num_set = ''
         self.one_unit = 0
@@ -24,6 +25,10 @@ class Loto:
         self.five_amount = 0
         self.carryover = 0
         self.sales = 0
+
+class Loto:
+    def __init__(self):
+        self.max_time = 0
 
     '''
     みずほ銀行の公式サイトで公開されている最新の回数を取得
@@ -48,6 +53,12 @@ class Loto:
         self.max_time = int(times)
 
     def parse(self, url):
+
+        # 第1264回ロト６,数字選択式全国自治宝くじ,平成30年3月29日,東京 宝くじドリーム館
+        # 支払期間,平成30年3月30日から平成31年3月29日まで
+        # 本数字,01,06,27,33,35,43,ボーナス数字,15
+
+        self.data = LotoData()
         filename = os.path.basename(url)
         try:
             urllib.request.urlretrieve(url, filename)
@@ -67,9 +78,11 @@ class Loto:
                 continue
 
             if head.find('１等') == 0:
-                self.one_unit = int(row[1].replace("該当なし", "0").replace("口", ""))
+                self.data.one_unit = int(row[1].replace("該当なし", "0").replace("口", ""))
                 self.one_amount = int(row[2].replace("該当なし", "0").replace("円", ""))
+                self.data.one_amount = int(row[2].replace("該当なし", "0").replace("円", ""))
                 # print(str(self.one_unit) + " " + str(self.one_amount))
+                print(str(self.data.one_unit) + " " + str(self.data.one_amount))
             if head.find('２等') == 0:
                 self.two_unit = int(row[1].replace("該当なし", "0").replace("口", ""))
                 self.two_amount = int(row[2].replace("該当なし", "0").replace("円", ""))
@@ -77,19 +90,33 @@ class Loto:
             if head.find('３等') == 0:
                 self.three_unit = int(row[1].replace("該当なし", "0").replace("口", ""))
                 self.three_amount = int(row[2].replace("該当なし", "0").replace("円", ""))
-                #print(str(self.three_unit) + " " + str(self.three_amount))
+                # print(str(self.three_unit) + " " + str(self.three_amount))
             if head.find('４等') == 0:
                 self.four_unit = int(row[1].replace("該当なし", "0").replace("口", ""))
                 self.four_amount = int(row[2].replace("該当なし", "0").replace("円", ""))
-                #print(str(self.four_unit) + " " + str(self.four_amount))
+                # print(str(self.four_unit) + " " + str(self.four_amount))
             if head.find('５等') == 0:
                 self.five_unit = int(row[1].replace("該当なし", "0").replace("口", ""))
                 self.five_amount = int(row[2].replace("該当なし", "0").replace("円", ""))
-                #print(str(self.five_unit) + " " + str(self.five_amount))
+                # print(str(self.five_unit) + " " + str(self.five_amount))
             if head.find('キャリーオーバー') == 0:
                 self.carryover = int(row[1].replace("円", ""))
-                #print(str(self.carryover))
+                # print(str(self.carryover))
             if head.find('販売実績額') == 0:
                 self.sales = int(row[1].replace("円", ""))
-                #print(str(self.sales))
+                # print(str(self.sales))
+            # if re.search("第\d回ロト６", head):
+            if head.find('第') == 0:
+                str_date = self.get_seireki(re.sub(r'(年|月)', "/", row[2]).replace("日", ""))
+                datetime.strptime(str_date, '%Y/%m/%d')
+                self.lottery_date = self.data.lottery_date = str_date
+                # print(str(self.data.lottery_date))
+
+        # 第1264回ロト６,数字選択式全国自治宝くじ,平成30年3月29日,東京 宝くじドリーム館
+    def get_seireki(self, str_wareki):
+        if str_wareki.find("平成29") == 0:
+            return str_wareki.replace("平成29", "2017")
+        if str_wareki.find("平成30") == 0:
+            return str_wareki.replace("平成30", "2018")
+        return ""
 
